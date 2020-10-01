dd3c95ac36a39df9bb3a101140e4b8748e08eb97
Maryjeba <maryjeba11@gmail.com>
2020-10-01 09:57:07 +0530

    Updates

---
+++ precache-manifest.800fd7f364fee2f71c177bed2959c927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51cc482ef15f07687f049c97636b8dad",
+    "url": "/recipe_app/index.html"
+  },
+  {
+    "revision": "07c7e70e684ed487f057",
+    "url": "/recipe_app/static/css/main.2f7155b5.chunk.css"
+  },
+  {
+    "revision": "84228da5c738054c36c6",
+    "url": "/recipe_app/static/js/2.42f53e1c.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/recipe_app/static/js/2.42f53e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c7e70e684ed487f057",
+    "url": "/recipe_app/static/js/main.9cf680ca.chunk.js"
+  },
+  {
+    "revision": "7581057984edd57b9500",
+    "url": "/recipe_app/static/js/runtime-main.52e9e9b6.js"
+  }
+]);
